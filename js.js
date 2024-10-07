30cbd8871fd14ffd16095652aac4d9b6df124a72
DESKTOP-F05VBP4\amoliaz <csi23-amoliaz@colegioaltair.net>
2024-10-07 14:40:31 +0200

    5

--- js.js
+++ js.js
@@ -1,11 +1,35 @@
-export function crearParrafoJS(){
+function agregarParrafo(){
+    //1
+    let contenedor = document.querySelector("#contenedor");
+    //2
     let p = document.createElement("p");
-    let contenedor = document.querySelector("body");
-
-    p.innerText="yayaya";
-    return p;
+    //3
+    p.innerText =  "lorem";
+    //4
+    contenedor.appendChild(p);
+    /*una sola linea
+    document.getElementById("#contenedor").innerHTML += "<p>sdbgafgb</p>"*/
 }
+///////////
+function agregarLista(){
+    let ul = document.querySelector("ul");
+   // let contenedor = document.querySelectorAll("ul")[0];
+    let producto = prompt("producto");
+    let producto2 = prompt("producto");
+    let  item1 = document.createElement("li");
+    let  item2 = document.createElement("li");
+    item1.innerText =  producto;
+    item2.innerHTML=  producto2;
+    ul.appendChild(item1);
+    ul.appendChild(item2);
+}
+///////////
+function creaConEstilo(){
+    let div = document.createElement('div');
+    div.innerHTML="andres molina";
+    div.style.color="red";
+    div.style.fontSize="300px";
+   // div.style="font-weight:bold,font...."
+   document.body.append(div);
 
-export function crearLista(){
-    
 }
