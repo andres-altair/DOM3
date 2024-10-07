a7ce83bebeeb5354b8f3e613c626136c78e8a2ec
DESKTOP-F05VBP4\amoliaz <csi23-amoliaz@colegioaltair.net>
2024-10-07 14:30:48 +0200

    3

---
+++ js.js
@@ -0,0 +1,8 @@
+function crearParrafoJS(){
+    let p = document.createElement("p");
+    let contenedor = document.querySelector("body");
+
+    p.innerText="yayaya";
+    contenedor.appendChild(p);
+
+}
