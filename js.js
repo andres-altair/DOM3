1ff5a403ae211ce17b3ded1aaa1eb25272734ba2
DESKTOP-F05VBP4\amoliaz <csi23-amoliaz@colegioaltair.net>
2024-10-07 14:37:03 +0200

    4

--- js.js
+++ js.js
@@ -1,8 +1,11 @@
-function crearParrafoJS(){
+export function crearParrafoJS(){
     let p = document.createElement("p");
     let contenedor = document.querySelector("body");
 
     p.innerText="yayaya";
-    contenedor.appendChild(p);
+    return p;
+}
 
+export function crearLista(){
+    
 }
---
+++ main.js
@@ -0,0 +1,6 @@
+import { crearParrafoJS } from "./js";
+
+function ponerParrafo(){
+    let body  = document.body.append(crearParrafoJS());
+}
+
